beb20fb11401bba86fbd649ed70ccb2cd88233bc
Wesley Tsang <tsangwe@oocl.com>
2020-04-10 00:22:18 +0800

    [Wesley] Pass test should_return_null_when_packingBoy_fetch_with_invalid_ticket

--- src/test/java/com/oocl/model/ParkingBoyTest.java
+++ src/test/java/com/oocl/model/ParkingBoyTest.java
@@ -13,12 +13,27 @@ public class ParkingBoyTest {
     }
 
     @Test
+    public void should_return_parkingTicket_for_each_when_park_multiple_car() {
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Assert.assertEquals(ParkingTicket.class, parkingBoy.park(new Car()).getClass());
+        Assert.assertEquals(ParkingTicket.class, parkingBoy.park(new Car()).getClass());
+        Assert.assertEquals(ParkingTicket.class, parkingBoy.park(new Car()).getClass());
+    }
+
+    @Test
     public void should_return_car_when_packingBoy_fetch_with_valid_ticket() {
         ParkingBoy parkingBoy = new ParkingBoy();
         Car carToPark = new Car();
         ParkingTicket parkingTicket = parkingBoy.park(carToPark);
         Car returnedCar = parkingBoy.fetch(parkingTicket);
         Assert.assertEquals(carToPark, returnedCar);
+    }
+
+    @Test
+    public void should_return_null_when_packingBoy_fetch_with_invalid_ticket() {
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car returnedCar = parkingBoy.fetch(new ParkingTicket(2));
+        Assert.assertEquals(null, returnedCar);
     }
 
 }
