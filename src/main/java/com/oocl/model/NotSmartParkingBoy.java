64a2fba8ea774792746094b3b2a6b9530d67cab4
Wesley Tsang <tsangwe@oocl.com>
2020-04-14 16:54:09 +0800

    [Wesley] Refactor, improve stream calls for NotSmartParkingBoy

--- src/main/java/com/oocl/model/NotSmartParkingBoy.java
+++ src/main/java/com/oocl/model/NotSmartParkingBoy.java
@@ -2,9 +2,6 @@ package com.oocl.model;
 
 import com.oocl.util.customException.ParkingLotIsFullException;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class NotSmartParkingBoy extends ParkingBoy {
     public NotSmartParkingBoy() {
         super();
@@ -12,12 +9,9 @@ public class NotSmartParkingBoy extends ParkingBoy {
 
     @Override
     protected ParkingLot selectParkingLot() throws ParkingLotIsFullException {
-        List<ParkingLot> nonFullParkingLots = this.parkingLots.stream()
+        return this.parkingLots.stream()
                 .filter(parkingLot -> !parkingLot.isFull())
-                .collect(Collectors.toList());
-        if (!nonFullParkingLots.isEmpty()) {
-            return nonFullParkingLots.get(0);
-        }
-        throw new ParkingLotIsFullException();
+                .findFirst()
+                .orElseThrow(ParkingLotIsFullException::new);
     }
 }
